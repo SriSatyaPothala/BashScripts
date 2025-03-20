9decd90a575b6175e0efc09c60d27e6fa438eb6c
SriSatyaPothala <srisatyapothala11@gmail.com>
2025-03-20 21:36:52 +0530

    Update textSubstitutionScript.sh

--- textSubstitutionScript.sh
+++ textSubstitutionScript.sh
@@ -6,7 +6,7 @@ FILE_PATH="/home/ec2-user/textfiles/file1.txt"
 
 #replacing all occurences of give with learning where there is welcome word but from 5th line
 #using sed welcome/I keeps the line that has no welcome word in the final output unline grep
-{ head -n 4 "$FILE_PATH" ;tail -n +5 "$FILE_PATH" | sed '/welcome/I s/give/learning/g'; } > tempoutput.txt
+{ head -n 4 "$FILE_PATH" ;tail -n +5 "$FILE_PATH" | sed '/welcome/I s/\bgive/\blearning/g'; } > tempoutput.txt
 mv tempoutput.txt "$FILE_PATH"
 
 echo "Substitution Successfull"
